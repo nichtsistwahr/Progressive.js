350e622d1eb8446031a931aab1e16e2f23ed2918
nichtsistwahr <github@prop23.de>
2015-04-09 14:49:19 +0200

    Rewrote style insertion; some stylistic changes; reduced code size by around 15%

--- progressive.js
+++ progressive.js
@@ -8,26 +8,23 @@ var Progressive = (function () {
 
 	"use strict";
 
-	var styleElem = document.createElement("style"),
-		animationSupport = false,
-		keyframePrefix = "",
-		getElements,
-		enhance;
+	var d = document,
+	    styleElem = d.createElement("style"),
+	    animationSupport = false,
+	    keyframePrefix = "";
 
 	// Detect type of element selector and choose fastest DOM selection method
-	getElements = function (selector) {
+	function getElements (selector) {
 		var selectorType = selector.match(/[^a-zA-Z0-9-]/g), elements;
-		// simple polyfill for getElementsByClassName
-		document.getElementsByClassName = document.getElementsByClassName || function(e){ return document.querySelectorAll("."+e) };
 
 		if (null === selectorType) {
-			elements = document.getElementsByTagName(selector);
-		} else if ((selectorType.length === 1) && (selectorType[0] === ".")) {
-			elements = document.getElementsByClassName(selector.slice(1));
+			elements = d.getElementsByTagName(selector);
+		} else if ((selectorType.length === 1) && (selectorType[0] === ".") && (d.getElementsByClassName !== undefined)) {
+			elements = d.getElementsByClassName(selector.slice(1));
 		} else if ((selectorType.length === 1) && (selectorType[0] === "#")) {
-			elements = [document.getElementById(selector.slice(1))];
+			elements = [d.getElementById(selector.slice(1))];
 		} else {
-			elements = document.querySelectorAll(selector);
+			elements = d.querySelectorAll(selector);
 		}
 		return elements;
 	}
@@ -41,69 +38,55 @@ var Progressive = (function () {
 	}
 
 	// The main `enhance` method to register callbacks that will be executed when certain DOM elements are inserted
-	enhance = function (enhancements) {
-		var ruleText,
-			styleRules = {},
-			enhancement,
-			onNodeInserted,
-			// The fallback function is executed on DOMContentLoaded. It will handle any elements not handled by the animation callbacks
-			fallback = function () {
-				var enhancement,
-					elems,
-					numElems,
-					i;
-				for (enhancement in enhancements) {
-					if (enhancements.hasOwnProperty(enhancement)) {
-						elems = getElements(enhancements[enhancement].selector || ("."+enhancements[enhancement].className));
-						numElems = elems.length;
-						if (!enhancements[enhancement].count || enhancements[enhancement].count < numElems) {
-							for (i = 0; i < numElems; i++) {
-								enhancements[enhancement].callback.call(elems[i]);
-							}
+	function enhance (enhancements) {
+		// The fallback function is executed on DOMContentLoaded. It will handle any elements not handled by the animation callbacks
+		function fallback () {
+			var enhancement,
+			    elems,
+			    numElems,
+			    i;
+			for (enhancement in enhancements) {
+				if (enhancements.hasOwnProperty(enhancement)) {
+					elems = getElements(enhancements[enhancement].selector);
+					numElems = elems.length;
+					if (!enhancements[enhancement].count || enhancements[enhancement].count < numElems) {
+						for (i = 0; i < numElems; i++) {
+							enhancements[enhancement].callback.call(elems[i]);
 						}
 					}
 				}
-			};
-		ruleText = "";
+			}
+		}
+
 		// This is used as a callback to the CSS animation events. It's used to fire the supplied enhancements, in the context of each element
-		onNodeInserted = function (e) {
+		function onNodeInserted (e) {
 			var enhancement = enhancements[e.animationName];
 			if (enhancement) {
 				enhancement.count = ++enhancement.count || 1;
 				enhancement.callback.call(e.target);
 			}
-		};
+		}
 
 		if (animationSupport) {
+			d.getElementsByTagName('script')[0].parentNode.appendChild(styleElem);
+			var styleSheet = styleElem.sheet;
 			// Build up a set of CSS rules to run animations on newly inserted elements
-			for (enhancement in enhancements) {
+			for (var enhancement in enhancements) {
 				if (enhancements.hasOwnProperty(enhancement)) {
-					ruleText += (enhancements[enhancement].selector || "."+enhancements[enhancement].className) + "{";
-					ruleText += keyframePrefix + "animation:" + enhancement + " 0.001s;";
-					ruleText += "}";
-					ruleText += "@" + keyframePrefix + "keyframes " + enhancement + "{from{opacity:0.99;}to{opacity:1;}}";
+					styleSheet.insertRule(enhancements[enhancement].selector + "{" + keyframePrefix + "animation:" + enhancement + " 0.001s;} ", styleSheet.cssRules.length);
+					styleSheet.insertRule("@" + keyframePrefix + "keyframes " + enhancement + "{from{opacity:0.99;}to{opacity:1;}}", styleSheet.cssRules.length);
 				}
 			}
 
-			styleRules = document.createTextNode(ruleText);
-			styleElem.type = "text/css";
-			if (styleElem.styleSheet) {
-				styleElem.styleSheet.cssText = styleRules.nodeValue;
-			} else {
-				styleElem.appendChild(styleRules);
-			}
-
-			document.getElementsByTagName("script")[0].parentNode.appendChild(styleElem);
-
 			// Register cross-browser CSS animation event handlers
-			document.addEventListener("animationstart", onNodeInserted, false);
-			document.addEventListener("webkitAnimationStart", onNodeInserted, false);
+			d.addEventListener("animationstart", onNodeInserted, false);
+			d.addEventListener("webkitAnimationStart", onNodeInserted, false);
 		} else {
 			// Register fallback event handlers
 			if (window.addEventListener) { window.addEventListener("DOMContentLoaded", fallback); }
 			else { window.attachEvent("onload", fallback); }
 		}
-	};
+	}
 
 	// Expose public methods
 	return {
